d10cd1ee7ead347fd07197dc406156da04e5078a
SzGabor <gabor.szendrei1@gmail.com>
2023-11-22 08:23:32 +0100

    query restructure

--- XMLTaskV9ZK10/domparse/DomQueryV9ZK10.java
+++ XMLTaskV9ZK10/domparse/DomQueryV9ZK10.java
@@ -22,176 +22,248 @@ public class DomQueryV9ZK10 {
 
         // Querying Vasarlo elements
         NodeList vasarloList = doc.getElementsByTagName("Vasarlo");
-        System.out.println("\nVásárlók:");
+        System.out.println("\n----------Vásárlók----------");
+
         for (int i = 0; i < vasarloList.getLength(); i++) {
             Node node = vasarloList.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
+
                 String email = element.getElementsByTagName("email_cim").item(0).getTextContent();
                 String name = element.getElementsByTagName("nev").item(0).getTextContent();
-                String phoneNumber = element.getElementsByTagName("Telefonszam").item(0).getTextContent();
-                System.out.println("Email: " + email + ", Name: " + name + ", Phone: " + phoneNumber);
+                NodeList phoneNumbers = element.getElementsByTagName("Telefonszam");
+
+                // Extracting address information
+                Element cimElement = (Element) element.getElementsByTagName("cim").item(0);
+                String orszag = cimElement.getElementsByTagName("orszag").item(0).getTextContent();
+                String isz = cimElement.getElementsByTagName("isz").item(0).getTextContent();
+                String varos = cimElement.getElementsByTagName("varos").item(0).getTextContent();
+                String uHsz = cimElement.getElementsByTagName("uHsz").item(0).getTextContent();
+
+                System.out.println("Email: " + email);
+                System.out.println("Név: " + name);
+                System.out.println("Cím: " + orszag + ", " + isz + " " + varos + ", " + uHsz);
+                System.out.println("Telefon:");
+
+                for (int j = 0; j < phoneNumbers.getLength(); j++) {
+                    String phoneNumber = phoneNumbers.item(j).getTextContent();
+                    System.out.println("- " + phoneNumber);
+                }
+
+                System.out.println();
             }
         }
 
-        // Querying Autos_ceg elements
-        NodeList autosCegList = doc.getElementsByTagName("Autos_ceg");
-        System.out.println("\nAutós cégek:");
+        // Querying AutosCeg elements
+        NodeList autosCegList = doc.getElementsByTagName("AutosCeg");
+        System.out.println("\n----------Autós cégek----------");
+
         for (int i = 0; i < autosCegList.getLength(); i++) {
             Node node = autosCegList.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
+
                 String companyCode = element.getAttribute("ceg_kod");
                 String companyName = element.getElementsByTagName("cegnev").item(0).getTextContent();
-                String employeeCount = element.getElementsByTagName("Dolgozok_szama").item(0).getTextContent();
-                System.out.println(
-                        "Cég kód: " + companyCode + ", Name: " + companyName + ", Employees: " + employeeCount);
+                String employeeCount = element.getElementsByTagName("dolgozokSzama").item(0).getTextContent();
+
+                System.out.println("Cég kód: " + companyCode);
+                System.out.println("Név: " + companyName);
+                System.out.println("Dolgozók száma: " + employeeCount);
+                System.out.println();
             }
         }
 
-        // Querying Autos_adatok elements
-        NodeList autosAdatokList = doc.getElementsByTagName("Autos_adatok");
-        System.out.println("\nAutós adatok:");
+        // Querying AutosAdatok elements
+        NodeList autosAdatokList = doc.getElementsByTagName("AutosAdatok");
+        System.out.println("\n----------Autós adatok----------");
+
         for (int i = 0; i < autosAdatokList.getLength(); i++) {
             Node node = autosAdatokList.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
-                String forgalmiKod = element.getAttribute("forgalmi_kod");
+
+                String forgalmiKod = element.getAttribute("forgalmiKod");
                 String adatokCompanyCode = element.getAttribute("ceg_kod");
                 String price = element.getElementsByTagName("ar").item(0).getTextContent();
-                String km = element.getElementsByTagName("km_ora").item(0).getTextContent();
+                String km = element.getElementsByTagName("kmOra").item(0).getTextContent();
                 String status = element.getElementsByTagName("statusz").item(0).getTextContent();
-                System.out
-                        .println("Forgalmi Kód: " + forgalmiKod + ", Cég kód: " + adatokCompanyCode + ", Price: "
-                                + price
-                                + ", KM: " + km + ", Status: " + status);
+
+                System.out.println("Forgalmi Kód: " + forgalmiKod);
+                System.out.println("Cég kód: " + adatokCompanyCode);
+                System.out.println("Price: " + price);
+                System.out.println("KM: " + km);
+                System.out.println("Status: " + status);
+                System.out.println();
             }
         }
 
-        // Querying Autos_tipus elements
-        NodeList autosTipusList = doc.getElementsByTagName("Autos_tipus");
-        System.out.println("\nAutós típusok:");
+        // Querying AutosTipus elements
+        NodeList autosTipusList = doc.getElementsByTagName("AutosTipus");
+        System.out.println("\n----------Autós típusok----------");
+
         for (int i = 0; i < autosTipusList.getLength(); i++) {
             Node node = autosTipusList.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
+
                 String tipusKod = element.getAttribute("tipus_kod");
-                String tipusForgalmiKod = element.getAttribute("forgalmi_kod");
-                String gyev = element.getElementsByTagName("Gy_ev").item(0).getTextContent();
+                String tipusForgalmiKod = element.getAttribute("forgalmiKod");
+                String gyev = element.getElementsByTagName("gyartasiEv").item(0).getTextContent();
                 String marka = element.getElementsByTagName("marka").item(0).getTextContent();
                 String nev = element.getElementsByTagName("nev").item(0).getTextContent();
-                System.out
-                        .println("Típus Kód: " + tipusKod + ", Forgalmi Kód: " + tipusForgalmiKod + ", Gyártási Év: "
-                                + gyev
-                                + ", Márka: " + marka + ", Név: " + nev);
+
+                System.out.println("Típus Kód: " + tipusKod);
+                System.out.println("Forgalmi Kód: " + tipusForgalmiKod);
+                System.out.println("Gyártási Év: " + gyev);
+                System.out.println("Márka: " + marka);
+                System.out.println("Név: " + nev);
+                System.out.println();
             }
         }
 
         // Querying Vasarlas elements
         NodeList vasarlasList = doc.getElementsByTagName("Vasarlas");
-        System.out.println("\nVásárlások:");
+        System.out.println("\n----------Vásárlások----------");
+
         for (int i = 0; i < vasarlasList.getLength(); i++) {
             Node node = vasarlasList.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
+
                 String vasarlasDatumkod = element.getAttribute("datumkod");
                 String vasarlasCompanyCode = element.getAttribute("ceg_kod");
                 String vasarlasVasarloKod = element.getAttribute("vasarlo_kod");
-                String kezdDatum = element.getElementsByTagName("kezd_datum").item(0).getTextContent();
-                System.out.println(
-                        "Dátumkód: " + vasarlasDatumkod + ", Cég kód: " + vasarlasCompanyCode + ", Vásárló Kód: "
-                                + vasarlasVasarloKod
-                                + ", Kezdő Dátum: " + kezdDatum);
+                String kezdDatum = element.getElementsByTagName("kezdDatum").item(0).getTextContent();
+
+                System.out.println("Dátumkód: " + vasarlasDatumkod);
+                System.out.println("Cég kód: " + vasarlasCompanyCode);
+                System.out.println("Vásárló Kód: " + vasarlasVasarloKod);
+                System.out.println("Kezdő Dátum: " + kezdDatum);
+                System.out.println();
             }
         }
 
         // Querying Szamlazas elements
         NodeList szamlazasList = doc.getElementsByTagName("Szamlazas");
-        System.out.println("\nSzámlázások:");
+        System.out.println("\n----------Számlázások----------");
+
         for (int i = 0; i < szamlazasList.getLength(); i++) {
             Node node = szamlazasList.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
+
                 String szamlaKod = element.getAttribute("szamlakod");
                 String szamlazasVasarloCode = element.getAttribute("vasarlo_kod");
-                String szamlaDatum = element.getElementsByTagName("szamla_datum").item(0).getTextContent();
+                String szamlaDatum = element.getElementsByTagName("szamlaDatum").item(0).getTextContent();
                 String vegOsszeg = element.getElementsByTagName("vegosszeg").item(0).getTextContent();
                 String jogsiszam = element.getElementsByTagName("jogsiszam").item(0).getTextContent();
-                System.out.println(
-                        "Számlakód: " + szamlaKod + ", Vásárló Kód: " + szamlazasVasarloCode + ", Számla Dátum: "
-                                + szamlaDatum
-                                + ", Végösszeg: " + vegOsszeg + ", Jogsiszám: " + jogsiszam);
+
+                System.out.println("Számlakód: " + szamlaKod);
+                System.out.println("Vásárló Kód: " + szamlazasVasarloCode);
+                System.out.println("Számla Dátum: " + szamlaDatum);
+                System.out.println("Végösszeg: " + vegOsszeg);
+                System.out.println("Jogsiszám: " + jogsiszam);
+                System.out.println();
             }
         }
 
         // Querying Autós_adatok and Autós_tipus for 1:1 relationship
-        NodeList autosAdatokList2 = doc.getElementsByTagName("Autos_adatok");
-        System.out.println("\nAutós adatok és az azokhoz tartozó típusok:");
+        NodeList autosAdatokList2 = doc.getElementsByTagName("AutosAdatok");
+        System.out.println("\n----------Autós adatok és az azokhoz tartozó típusok----------");
+
         for (int i = 0; i < autosAdatokList2.getLength(); i++) {
             Node node = autosAdatokList2.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element autosAdatokElement = (Element) node;
                 String forgalmiKod = autosAdatokElement.getAttribute("forgalmi_kod");
                 String adatokCompanyCode = autosAdatokElement.getAttribute("ceg_kod");
                 String ar = autosAdatokElement.getElementsByTagName("ar").item(0).getTextContent();
-                String kmOra = autosAdatokElement.getElementsByTagName("km_ora").item(0).getTextContent();
+                String AutosAdatok = autosAdatokElement.getElementsByTagName("kmOra").item(0).getTextContent();
                 String status = autosAdatokElement.getElementsByTagName("statusz").item(0).getTextContent();
 
-                // Querying Autos_tipus for the same forgalmi_kod
-                NodeList autosTipusList2 = doc.getElementsByTagName("Autos_tipus");
+                // Querying AutosTipus for the same forgalmiKod
+                NodeList autosTipusList2 = doc.getElementsByTagName("AutosTipus");
                 for (int j = 0; j < autosTipusList2.getLength(); j++) {
                     Node autosTipusNode = autosTipusList2.item(j);
+
                     if (autosTipusNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element autosTipusElement = (Element) autosTipusNode;
                         String tipusForgalmiKod = autosTipusElement.getAttribute("forgalmi_kod");
+
                         if (forgalmiKod.equals(tipusForgalmiKod)) {
-                            String gyev = autosTipusElement.getElementsByTagName("Gy_ev").item(0).getTextContent();
+                            String gyev = autosTipusElement.getElementsByTagName("gyartasiEv").item(0).getTextContent();
                             String marka = autosTipusElement.getElementsByTagName("marka").item(0).getTextContent();
                             String nev = autosTipusElement.getElementsByTagName("nev").item(0).getTextContent();
 
-                            System.out.println("Forgalmi Kód: " + forgalmiKod + ", Cég kód: " + adatokCompanyCode +
-                                    ", Ár: " + ar + ", KM óra: " + kmOra + ", Státusz: " + status +
-                                    ", Gyártási Év: " + gyev + ", Márka: " + marka + ", Név: " + nev);
+                            System.out.println("Forgalmi Kód: " + forgalmiKod);
+                            System.out.println("Cég kód: " + adatokCompanyCode);
+                            System.out.println("Ár: " + ar);
+                            System.out.println("KM óra: " + AutosAdatok);
+                            System.out.println("Státusz: " + status);
+                            System.out.println("Gyártási Év: " + gyev);
+                            System.out.println("Márka: " + marka);
+                            System.out.println("Név: " + nev);
+                            System.out.println();
                         }
                     }
                 }
             }
         }
 
-        // Querying Autos_ceg and Autos_adatok for 1:n relationship
-        NodeList autosCegList1 = doc.getElementsByTagName("Autos_ceg");
-        System.out.println("\nAutós cégek és az azokhoz tartozó adatok:");
+        // Querying AutosCeg and AutosAdatok for 1:n relationship
+        NodeList autosCegList1 = doc.getElementsByTagName("AutosCeg");
+        System.out.println("\n----------Autós cégek és az azokhoz tartozó adatok----------");
+
         for (int i = 0; i < autosCegList1.getLength(); i++) {
             Node node = autosCegList1.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element autosCegElement = (Element) node;
                 String companyCode = autosCegElement.getAttribute("ceg_kod");
                 String companyName = autosCegElement.getElementsByTagName("cegnev").item(0).getTextContent();
 
-                // Querying Autos_adatok for the same ceg_kod
-                NodeList autosAdatokList1 = doc.getElementsByTagName("Autos_adatok");
+                // Querying AutosAdatok for the same ceg_kod
+                NodeList autosAdatokList1 = doc.getElementsByTagName("AutosAdatok");
                 for (int j = 0; j < autosAdatokList1.getLength(); j++) {
                     Node autosAdatokNode = autosAdatokList1.item(j);
+
                     if (autosAdatokNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element autosAdatokElement = (Element) autosAdatokNode;
                         String adatokCompanyCode = autosAdatokElement.getAttribute("ceg_kod");
+
                         if (companyCode.equals(adatokCompanyCode)) {
                             String ar = autosAdatokElement.getElementsByTagName("ar").item(0).getTextContent();
-                            String kmOra = autosAdatokElement.getElementsByTagName("km_ora").item(0).getTextContent();
+                            String AutosAdatok = autosAdatokElement.getElementsByTagName("kmOra").item(0)
+                                    .getTextContent();
                             String status = autosAdatokElement.getElementsByTagName("statusz").item(0).getTextContent();
 
-                            System.out.println("Cég kód: " + companyCode + ", Cég név: " + companyName +
-                                    ", Ár: " + ar + ", KM óra: " + kmOra + ", Státusz: " + status);
+                            System.out.println("Cég kód: " + companyCode);
+                            System.out.println("Cég név: " + companyName);
+                            System.out.println("Ár: " + ar);
+                            System.out.println("KM óra: " + AutosAdatok);
+                            System.out.println("Státusz: " + status);
+                            System.out.println();
                         }
                     }
                 }
             }
         }
+
         // Querying Vasarlo and Vasarlas for n:m relationship
         NodeList vasarloList1 = doc.getElementsByTagName("Vasarlo");
-        System.out.println("\nVásárlók és az azokhoz tartozó vásárlások:");
+        System.out.println("\n----------Vásárlók és az azokhoz tartozó vásárlások----------");
+
         for (int i = 0; i < vasarloList1.getLength(); i++) {
             Node node = vasarloList1.item(i);
+
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element vasarloElement = (Element) node;
                 String vasarloKod = vasarloElement.getAttribute("jogkod");
@@ -202,20 +274,24 @@ public class DomQueryV9ZK10 {
                 NodeList vasarlasList1 = doc.getElementsByTagName("Vasarlas");
                 for (int j = 0; j < vasarlasList1.getLength(); j++) {
                     Node vasarlasNode = vasarlasList1.item(j);
+
                     if (vasarlasNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element vasarlasElement = (Element) vasarlasNode;
                         String vasarlasVasarloKod = vasarlasElement.getAttribute("vasarlo_kod");
+
                         if (vasarloKod.equals(vasarlasVasarloKod)) {
                             String vasarlasDatumkod = vasarlasElement.getAttribute("datumkod");
                             String vasarlasCompanyCode = vasarlasElement.getAttribute("ceg_kod");
-                            String kezdDatum = vasarlasElement.getElementsByTagName("kezd_datum").item(0)
+                            String kezdDatum = vasarlasElement.getElementsByTagName("kezdDatum").item(0)
                                     .getTextContent();
 
-                            System.out.println("Vásárló Kód: " + vasarloKod + ", Név: " + vasarloName +
-                                    ", Email: " + vasarloEmail +
-                                    ", Vásárlás Dátumkód: " + vasarlasDatumkod + ", Cég kód: "
-                                    + vasarlasCompanyCode +
-                                    ", Első vásárlás: " + kezdDatum);
+                            System.out.println("Vásárló Kód: " + vasarloKod);
+                            System.out.println("Név: " + vasarloName);
+                            System.out.println("Email: " + vasarloEmail);
+                            System.out.println("Vásárlás Dátumkód: " + vasarlasDatumkod);
+                            System.out.println("Cég kód: " + vasarlasCompanyCode);
+                            System.out.println("Első vásárlás: " + kezdDatum);
+                            System.out.println();
                         }
                     }
                 }
