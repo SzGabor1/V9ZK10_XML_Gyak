c545da10df6962459fa51e21805122483491c252
SzGabor <gabor.szendrei1@gmail.com>
2023-12-06 17:57:14 +0100

    Query javitas

--- XMLTaskV9ZK10/hu/domparse/V9ZK10/DomQueryV9ZK10_clean.java
+++ XMLTaskV9ZK10/hu/domparse/V9ZK10/DomQueryV9ZK10_clean.java
@@ -25,8 +25,6 @@ public class DomQueryV9ZK10 {
         NodeList vasarloList = doc.getElementsByTagName("Vasarlo");
         System.out.println("\n----------Vásárlók----------");
 
-        StringBuilder outputBuilder = new StringBuilder();
-
         for (int i = 0; i < vasarloList.getLength(); i++) {
             Node node = vasarloList.item(i);
 
@@ -46,31 +44,23 @@ public class DomQueryV9ZK10 {
                 String uHsz = cimElement.getElementsByTagName("uHsz").item(0).getTextContent();
 
                 // Adataink kiírása
-                outputBuilder.append("<Vasarlo>\n");
-                outputBuilder.append("  <Email>").append(email).append("</Email>\n");
-                outputBuilder.append("  <Nev>").append(name).append("</Nev>\n");
-                outputBuilder.append("  <Cim>").append(orszag).append(", ").append(isz).append(" ").append(varos)
-                        .append(", ").append(uHsz).append("</Cim>\n");
-                outputBuilder.append("  <Telefon>\n");
+                System.out.println("Email: " + email);
+                System.out.println("Név: " + name);
+                System.out.println("Cím: " + orszag + ", " + isz + " " + varos + ", " + uHsz);
+                System.out.println("Telefon:");
 
                 for (int j = 0; j < phoneNumbers.getLength(); j++) {
                     String phoneNumber = phoneNumbers.item(j).getTextContent();
-                    outputBuilder.append("    <Number>").append(phoneNumber).append("</Number>\n");
+                    System.out.println("- " + phoneNumber);
                 }
 
-                outputBuilder.append("  </Telefon>\n");
-                outputBuilder.append("</Vasarlo>\n");
+                System.out.println();
             }
         }
-
-        // Kiírás
-        System.out.println(outputBuilder.toString());
 
         // Autós cégek lekérdezése
         NodeList autosCegList = doc.getElementsByTagName("AutosCeg");
         System.out.println("\n----------Autós cégek----------");
-
-        StringBuilder outputBuilder1 = new StringBuilder();
 
         for (int i = 0; i < autosCegList.getLength(); i++) {
             Node node = autosCegList.item(i);
@@ -82,22 +72,16 @@ public class DomQueryV9ZK10 {
                 String companyName = element.getElementsByTagName("cegnev").item(0).getTextContent();
                 String employeeCount = element.getElementsByTagName("dolgozokSzama").item(0).getTextContent();
 
-                outputBuilder1.append("<AutosCeg>\n");
-                outputBuilder1.append("  <CegKod>").append(companyCode).append("</CegKod>\n");
-                outputBuilder1.append("  <Nev>").append(companyName).append("</Nev>\n");
-                outputBuilder1.append("  <DolgozokSzama>").append(employeeCount).append("</DolgozokSzama>\n");
-                outputBuilder1.append("</AutosCeg>\n");
+                System.out.println("Cég kód: " + companyCode);
+                System.out.println("Név: " + companyName);
+                System.out.println("Dolgozók száma: " + employeeCount);
+                System.out.println();
             }
         }
-
-        // Kiírás
-        System.out.println(outputBuilder1.toString());
 
         // Autós adatok lekérdezése
         NodeList autosAdatokList = doc.getElementsByTagName("AutosAdatok");
         System.out.println("\n----------Autós adatok----------");
-
-        StringBuilder outputBuilder5 = new StringBuilder();
 
         for (int i = 0; i < autosAdatokList.getLength(); i++) {
             Node node = autosAdatokList.item(i);
@@ -105,30 +89,24 @@ public class DomQueryV9ZK10 {
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
 
-                String forgalmiKod = element.getAttribute("forgalmi_kod");
+                String forgalmiKod = element.getAttribute("forgalmiKod");
                 String adatokCompanyCode = element.getAttribute("ceg_kod");
                 String price = element.getElementsByTagName("ar").item(0).getTextContent();
                 String km = element.getElementsByTagName("kmOra").item(0).getTextContent();
                 String status = element.getElementsByTagName("statusz").item(0).getTextContent();
 
-                outputBuilder5.append("<AutosAdatok>\n");
-                outputBuilder5.append("  <ForgalmiKod>").append(forgalmiKod).append("</ForgalmiKod>\n");
-                outputBuilder5.append("  <CegKod>").append(adatokCompanyCode).append("</CegKod>\n");
-                outputBuilder5.append("  <Ar>").append(price).append("</Ar>\n");
-                outputBuilder5.append("  <Km>").append(km).append("</Km>\n");
-                outputBuilder5.append("  <Statusz>").append(status).append("</Statusz>\n");
-                outputBuilder5.append("</AutosAdatok>\n");
+                System.out.println("Forgalmi Kód: " + forgalmiKod);
+                System.out.println("Cég kód: " + adatokCompanyCode);
+                System.out.println("Ár: " + price);
+                System.out.println("KM: " + km);
+                System.out.println("Státusz: " + status);
+                System.out.println();
             }
         }
-
-        // Kiírás
-        System.out.println(outputBuilder5.toString());
 
         // Autós típusok lekérdezése
         NodeList autosTipusList = doc.getElementsByTagName("AutosTipus");
         System.out.println("\n----------Autós típusok----------");
-
-        StringBuilder outputBuilder3 = new StringBuilder();
 
         for (int i = 0; i < autosTipusList.getLength(); i++) {
             Node node = autosTipusList.item(i);
@@ -137,29 +115,23 @@ public class DomQueryV9ZK10 {
                 Element element = (Element) node;
 
                 String tipusKod = element.getAttribute("tipus_kod");
-                String tipusForgalmiKod = element.getAttribute("forgalmi_kod");
+                String tipusForgalmiKod = element.getAttribute("forgalmiKod");
                 String gyev = element.getElementsByTagName("gyartasiEv").item(0).getTextContent();
                 String marka = element.getElementsByTagName("marka").item(0).getTextContent();
                 String nev = element.getElementsByTagName("nev").item(0).getTextContent();
 
-                outputBuilder3.append("<AutosTipus>\n");
-                outputBuilder3.append("  <TipusKod>").append(tipusKod).append("</TipusKod>\n");
-                outputBuilder3.append("  <ForgalmiKod>").append(tipusForgalmiKod).append("</ForgalmiKod>\n");
-                outputBuilder3.append("  <GyartasiEv>").append(gyev).append("</GyartasiEv>\n");
-                outputBuilder3.append("  <Marka>").append(marka).append("</Marka>\n");
-                outputBuilder3.append("  <Nev>").append(nev).append("</Nev>\n");
-                outputBuilder3.append("</AutosTipus>\n");
+                System.out.println("Típus Kód: " + tipusKod);
+                System.out.println("Forgalmi Kód: " + tipusForgalmiKod);
+                System.out.println("Gyártási Év: " + gyev);
+                System.out.println("Márka: " + marka);
+                System.out.println("Név: " + nev);
+                System.out.println();
             }
         }
-
-        // Kiírás
-        System.out.println(outputBuilder3.toString());
 
         // Vásárlások lekérdezése
         NodeList vasarlasList = doc.getElementsByTagName("Vasarlas");
         System.out.println("\n----------Vásárlások----------");
-
-        StringBuilder outputBuilder6 = new StringBuilder();
 
         for (int i = 0; i < vasarlasList.getLength(); i++) {
             Node node = vasarlasList.item(i);
@@ -172,22 +144,17 @@ public class DomQueryV9ZK10 {
                 String vasarlasVasarloKod = element.getAttribute("vasarlo_kod");
                 String kezdDatum = element.getElementsByTagName("kezdDatum").item(0).getTextContent();
 
-                outputBuilder6.append("<Vasarlas>\n");
-                outputBuilder6.append("  <Datumkod>").append(vasarlasDatumkod).append("</Datumkod>\n");
-                outputBuilder6.append("  <CegKod>").append(vasarlasCompanyCode).append("</CegKod>\n");
-                outputBuilder6.append("  <VasarloKod>").append(vasarlasVasarloKod).append("</VasarloKod>\n");
-                outputBuilder6.append("  <KezdDatum>").append(kezdDatum).append("</KezdDatum>\n");
-                outputBuilder6.append("</Vasarlas>\n");
+                System.out.println("Dátumkód: " + vasarlasDatumkod);
+                System.out.println("Cég kód: " + vasarlasCompanyCode);
+                System.out.println("Vásárló Kód: " + vasarlasVasarloKod);
+                System.out.println("Kezdő Dátum: " + kezdDatum);
+                System.out.println();
             }
         }
 
-        // Kiírás
-        System.out.println(outputBuilder6.toString());
         // Számlázások lekérdezése, ahol a végösszeg nagyobb mint 60.000
         NodeList szamlazasList = doc.getElementsByTagName("Szamlazas");
         System.out.println("\n----------Számlázások ami nagyobb mint 60.000----------");
-
-        StringBuilder outputBuilder4 = new StringBuilder();
 
         for (int i = 0; i < szamlazasList.getLength(); i++) {
             Node node = szamlazasList.item(i);
@@ -203,25 +170,19 @@ public class DomQueryV9ZK10 {
 
                 // Ellenőrizd, hogy a végösszeg nagyobb vagy egyenlő mint 60000
                 if (Integer.parseInt(vegOsszeg) >= 60000) {
-                    outputBuilder4.append("<Számlázás>\n");
-                    outputBuilder4.append("  <Számlakód>").append(szamlaKod).append("</Számlakód>\n");
-                    outputBuilder4.append("  <VásárlóKód>").append(szamlazasVasarloCode).append("</VásárlóKód>\n");
-                    outputBuilder4.append("  <SzámlaDátum>").append(szamlaDatum).append("</SzámlaDátum>\n");
-                    outputBuilder4.append("  <Végösszeg>").append(vegOsszeg).append("</Végösszeg>\n");
-                    outputBuilder4.append("  <Jogsiszám>").append(jogsiszam).append("</Jogsiszám>\n");
-                    outputBuilder4.append("</Számlázás>\n");
+                    System.out.println("Számlakód: " + szamlaKod);
+                    System.out.println("Vásárló Kód: " + szamlazasVasarloCode);
+                    System.out.println("Számla Dátum: " + szamlaDatum);
+                    System.out.println("Végösszeg: " + vegOsszeg);
+                    System.out.println("Jogsiszám: " + jogsiszam);
+                    System.out.println();
                 }
             }
         }
 
-        // Kiírás
-        System.out.println(outputBuilder4.toString());
-
         // Autós_adatok és Autós_tipus lekérdezése 1:1 kapcsolatban
         NodeList autosAdatokList2 = doc.getElementsByTagName("AutosAdatok");
         System.out.println("\n----------Autós adatok és az azokhoz tartozó típusok----------");
-
-        StringBuilder outputBuilder2 = new StringBuilder();
 
         for (int i = 0; i < autosAdatokList2.getLength(); i++) {
             Node node = autosAdatokList2.item(i);
@@ -248,30 +209,24 @@ public class DomQueryV9ZK10 {
                             String marka = autosTipusElement.getElementsByTagName("marka").item(0).getTextContent();
                             String nev = autosTipusElement.getElementsByTagName("nev").item(0).getTextContent();
 
-                            outputBuilder2.append("<AutósAdatok>\n");
-                            outputBuilder2.append("  <ForgalmiKód>").append(forgalmiKod).append("</ForgalmiKód>\n");
-                            outputBuilder2.append("  <CégKód>").append(adatokCompanyCode).append("</CégKód>\n");
-                            outputBuilder2.append("  <Ár>").append(ar).append("</Ár>\n");
-                            outputBuilder2.append("  <KMóra>").append(AutosAdatok).append("</KMóra>\n");
-                            outputBuilder2.append("  <Státusz>").append(status).append("</Státusz>\n");
-                            outputBuilder2.append("  <GyártásiÉv>").append(gyev).append("</GyártásiÉv>\n");
-                            outputBuilder2.append("  <Márka>").append(marka).append("</Márka>\n");
-                            outputBuilder2.append("  <Név>").append(nev).append("</Név>\n");
-                            outputBuilder2.append("</AutósAdatok>\n");
+                            System.out.println("Forgalmi Kód: " + forgalmiKod);
+                            System.out.println("Cég kód: " + adatokCompanyCode);
+                            System.out.println("Ár: " + ar);
+                            System.out.println("KM óra: " + AutosAdatok);
+                            System.out.println("Státusz: " + status);
+                            System.out.println("Gyártási Év: " + gyev);
+                            System.out.println("Márka: " + marka);
+                            System.out.println("Név: " + nev);
+                            System.out.println();
                         }
                     }
                 }
             }
         }
 
-        // Kiírás
-        System.out.println(outputBuilder2.toString());
-
         // Autós_cégek és Autós_adatok lekérdezése 1:n kapcsolatban
         NodeList autosCegList1 = doc.getElementsByTagName("AutosCeg");
         System.out.println("\n----------Autós cégek és az azokhoz tartozó adatok----------");
-
-        StringBuilder outputBuilder7 = new StringBuilder();
 
         for (int i = 0; i < autosCegList1.getLength(); i++) {
             Node node = autosCegList1.item(i);
@@ -296,27 +251,21 @@ public class DomQueryV9ZK10 {
                                     .getTextContent();
                             String status = autosAdatokElement.getElementsByTagName("statusz").item(0).getTextContent();
 
-                            outputBuilder7.append("<AutósCég>\n");
-                            outputBuilder7.append("  <CégKód>").append(companyCode).append("</CégKód>\n");
-                            outputBuilder7.append("  <CégNév>").append(companyName).append("</CégNév>\n");
-                            outputBuilder7.append("  <Ár>").append(ar).append("</Ár>\n");
-                            outputBuilder7.append("  <KMóra>").append(AutosAdatok).append("</KMóra>\n");
-                            outputBuilder7.append("  <Státusz>").append(status).append("</Státusz>\n");
-                            outputBuilder7.append("</AutósCég>\n");
+                            System.out.println("Cég kód: " + companyCode);
+                            System.out.println("Cég név: " + companyName);
+                            System.out.println("Ár: " + ar);
+                            System.out.println("KM óra: " + AutosAdatok);
+                            System.out.println("Státusz: " + status);
+                            System.out.println();
                         }
                     }
                 }
             }
         }
 
-        // Kiírás
-        System.out.println(outputBuilder7.toString());
-
         // Vásárló és vásárlás lekérdezése n:m kapcsolattal
         NodeList vasarloList1 = doc.getElementsByTagName("Vasarlo");
         System.out.println("\n----------Vásárlók és az azokhoz tartozó vásárlások----------");
-
-        StringBuilder outputBuilder8 = new StringBuilder();
 
         for (int i = 0; i < vasarloList1.getLength(); i++) {
             Node node = vasarloList1.item(i);
@@ -342,24 +291,18 @@ public class DomQueryV9ZK10 {
                             String kezdDatum = vasarlasElement.getElementsByTagName("kezdDatum").item(0)
                                     .getTextContent();
 
-                            outputBuilder8.append("<Vásárló>\n");
-                            outputBuilder8.append("  <VásárlóKód>").append(vasarloKod).append("</VásárlóKód>\n");
-                            outputBuilder8.append("  <Név>").append(vasarloName).append("</Név>\n");
-                            outputBuilder8.append("  <Email>").append(vasarloEmail).append("</Email>\n");
-                            outputBuilder8.append("  <Vásárlás>\n");
-                            outputBuilder8.append("    <Dátumkód>").append(vasarlasDatumkod).append("</Dátumkód>\n");
-                            outputBuilder8.append("    <CégKód>").append(vasarlasCompanyCode).append("</CégKód>\n");
-                            outputBuilder8.append("    <ElsőVásárlás>").append(kezdDatum).append("</ElsőVásárlás>\n");
-                            outputBuilder8.append("  </Vásárlás>\n");
-                            outputBuilder8.append("</Vásárló>\n");
+                            System.out.println("Vásárló Kód: " + vasarloKod);
+                            System.out.println("Név: " + vasarloName);
+                            System.out.println("Email: " + vasarloEmail);
+                            System.out.println("Vásárlás Dátumkód: " + vasarlasDatumkod);
+                            System.out.println("Cég kód: " + vasarlasCompanyCode);
+                            System.out.println("Első vásárlás: " + kezdDatum);
+                            System.out.println();
                         }
                     }
                 }
             }
         }
-
-        // Kiírás
-        System.out.println(outputBuilder8.toString());
 
     }
 }
